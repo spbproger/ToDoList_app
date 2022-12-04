66dcb1fbc89b5c52d9ba6b1385d6afa7984366e0
spbproger <zemli2016@yandex.ru>
2022-12-04 17:20:46 +0300

    Add authenticate, login at core/login

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
-from django.contrib.auth import get_user_model
+from rest_framework.response import Response
 
-from .serializers import RegistrationSerializer
+
+from django.contrib.auth import get_user_model, login
+
+from .serializers import RegistrationSerializer, LoginSerializer
 
 USER_MODEL = get_user_model()
 
@@ -9,3 +12,15 @@ USER_MODEL = get_user_model()
 class RegistrationView(generics.CreateAPIView):
     model = USER_MODEL
     serializer_class = RegistrationSerializer
+
+
+class LoginView(generics.CreateAPIView):
+    model = USER_MODEL
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
